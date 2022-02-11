71858fb74636cb8c489ed15494c4f4ba63388b28
Franco Quintana <franco.n.quintana@gmail.com>
2022-02-11 10:33:42 -0300

    Update swapi.js

--- pages/api/swapi.js
+++ pages/api/swapi.js
@@ -4,7 +4,7 @@ export default (req, res) => {
     redirect: 'follow'
   };
 
-  fetch(process.env.SWAPI_ENDPOINT + "/people/1/", requestOptions)
+  fetch(process.env.SWAPI_ENDPOINT + "/api/people/1/", requestOptions)
     .then(response => response.text())
     .then(result => res.status(200).json(result))
     .catch(error => res.status(500).json({ error, txt: 'fail' }));
